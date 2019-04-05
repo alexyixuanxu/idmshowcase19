53bc39b00be97587b5e1717c4bd1aad682931656
Alex Yixuan Xu <yx1137@nyu.edu>
2019-04-05 16:50:46 -0400

    added project page

---
+++ js/index.js
@@ -0,0 +1,94 @@
+var Shuffle = window.Shuffle;
+var element = document.querySelector('.my-shuffle-container');
+var sizer = element.querySelector('.my-sizer-element');
+
+var shuffleInstance = new Shuffle(element, {
+  itemSelector: '.grid__brick',
+  sizer: sizer, // could also be a selector: '.my-sizer-element'
+  initialSort: null
+});
+// shuffleInstance.filter('animal');
+$("#all").on("click", function(){
+   shuffleInstance.filter();
+});
+$("#btn-print").on("click", function(){
+   shuffleInstance.filter('Print');
+});
+$("#btn-installation").on("click", function(){
+   shuffleInstance.filter('Installation');
+});
+$("#btn-VRAR").on("click", function(){
+   shuffleInstance.filter('VR/AR');
+});
+$("#random").on("click", function() {
+  shuffleInstance.sort({randomize:true})
+});
+setTimeout(function() {shuffleInstance.sort({randomize:true})}, 1000)
+// setTimeout(function() {shuffleInstance.sort({randomize:false})}, 1500)
+
+
+var Demo = function (element) {
+  this.element = element;
+
+  this.shuffle = new Shuffle(element, {
+    itemSelector: '.grid__brick',
+    sizer: element.querySelector('.my-sizer-element'),
+  });
+
+  // Log events.
+  // this.addShuffleEventListeners();
+  //
+  // this._activeFilters = [];
+  //
+  // this.addFilterButtons();
+  // this.addSorting();
+  this.addSearchFilter();
+
+  this.mode = 'exclusive';
+};
+
+
+
+
+// Advanced filtering
+Demo.prototype.addSearchFilter = function () {
+  var searchInput = document.querySelector('.js-shuffle-search');
+
+  if (!searchInput) {
+    return;
+  }
+
+  searchInput.addEventListener('keyup', this._handleSearchKeyup.bind(this));
+};
+
+/**
+ * Filter the shuffle instance by items with a title that matches the search input.
+ * @param {Event} evt Event object.
+ */
+Demo.prototype._handleSearchKeyup = function (evt) {
+  var searchText = evt.target.value.toLowerCase();
+
+  this.shuffle.filter(function (element, shuffle) {
+
+    // If there is a current filter applied, ignore elements that don't match it.
+    if (shuffle.group !== Shuffle.ALL_ITEMS) {
+      // Get the item's groups.
+      var groups = JSON.parse(element.getAttribute('data-groups'));
+      var isElementInCurrentGroup = groups.indexOf(shuffle.group) !== -1;
+
+      // Only search elements in the current group
+      if (!isElementInCurrentGroup) {
+        return false;
+      }
+    }
+
+    var titleElement = element.querySelector('.card-body');
+    var titleText = titleElement.textContent.toLowerCase().trim();
+
+    return titleText.indexOf(searchText) !== -1;
+  });
+};
+
+document.addEventListener('DOMContentLoaded', function () {
+  window.demo = new Demo(document.getElementById('grid'));
+});
---
+++ js/padding-grid.js
@@ -0,0 +1,37 @@
+window.shuffleInstance = new window.Shuffle(document.getElementById('grid'), {
+  itemSelector: '.grid__brick',
+  sizer: '.my-sizer-element',
+  initialSort: {randomize:false}
+});
+
+
+$(document).ready(function() {
+
+});
+
+var resizeTimer;
+
+$(window).resize(function() {
+  window.shuffleInstance.sort({randomize:false});
+  clearTimeout(resizeTimer);
+  resizeTimer = setTimeout(function() {
+
+    shuffleInstance.sort({randomize:true});
+
+  }, 250);
+  // setTimeout({shuffleInstance.sort({randomize:true})}, 3000));
+})
+
+var showcaseLogo = document.getElementById('showcase-logo');
+
+setTimeout(function() {
+  showcaseLogo.src = 'img/idmstickertransparent.png'
+}, 3250)
+
+showcaseLogo.addEventListener('mouseover', function() {
+    showcaseLogo.src = 'img/idmstickerblinkblack.gif';
+});
+
+showcaseLogo.addEventListener('mouseout', function() {
+    showcaseLogo.src = 'img/idmstickertransparent.png';
+});
